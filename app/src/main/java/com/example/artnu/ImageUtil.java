f77a5f9803af6525041f90f96c2d71130310e14c
Erwan <erwan.pannier@centralesupelec.fr>
2019-10-10 02:16:12 +0200

    fix screenshot on erwan's phone

--- app/src/main/java/com/example/artnu/ImageUtil.java
+++ app/src/main/java/com/example/artnu/ImageUtil.java
@@ -5,10 +5,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
 
@@ -25,18 +28,37 @@ public class ImageUtil {
         contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
         contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH,   "DCIM/");
 
-        Uri uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
-
-        BufferedOutputStream oStream = null;
+        BufferedOutputStream oStream ;
         try {
+            Uri uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
             oStream = new BufferedOutputStream(resolver.openOutputStream(uri));
             finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, oStream);
             oStream.close();
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            SaveImage(finalBitmap);
         }
 
+    }
+
+    private static void SaveImage(Bitmap finalBitmap) {
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-" + n + ".jpg";
+        File file = new File(myDir, fname);
+        if (file.exists()) file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
